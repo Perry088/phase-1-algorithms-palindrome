c9231a181c1024f18142a8a477c4cec8d03c5302
Trenton Perry <trentperry08@gmail.com>
2023-10-04 14:56:38 -0800

    made function to check palincrome

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+// write a function that wil ltell you whether or not a word is an palindrome
+
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word === word.split('').reverse().join('')){
+    return true
+  }else{
+    return false
+  }
+
 }
 
 /* 
   Add your pseudocode here
+   check if string is a palindrome
+
+   return true or false
 */
 
 /*
   Add written explanation of your solution here
+  function takes a word, splits it up and reverses it, and then compares it to the original word to check if it is the same
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +35,4 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
